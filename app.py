c7430529e586b17bd8841f77a788c95dd60a5475
Gouher Danish <gouherdanishiitkgp@gmail.com>
2024-09-21 01:09:55 +0530

    Modified app.py file

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
+from segmentation.raster_segmentation import RasterSegmentation
 from segmentation.static_segmentation import StaticSegmentation
 
 import streamlit as st
 import pydeck as pdk
 
-def get_map_layers():
-    # dtm_path = './data/n12_e077_1arc_v3.tif'
-    file_path = './output/low_lying_pts.csv'
-    seg = StaticSegmentation(file_path=file_path)
-    df = seg.segment()
+def get_initial_view_state():
+    return pdk.ViewState(
+        latitude=12.969,
+        longitude=77.739661,
+        zoom=15,
+        pitch=50,
+    )
+
+def get_map_layers(df):
     return  {
         "Low Lying Areas":
             pdk.Layer(
@@ -19,26 +24,34 @@ def get_map_layers():
             ),
     }
 
-if __name__=='__main__':
-    layers = get_map_layers()
+def select_layers(layers):
     st.sidebar.markdown("### Map Layers")
     selected_layers = [
         layer
         for layer_name, layer in layers.items()
         if st.sidebar.checkbox(layer_name, True)
     ]
-    if selected_layers:
-        st.pydeck_chart(
-            pdk.Deck(
-                map_style=None,
-                initial_view_state=pdk.ViewState(
-                    latitude=12.97,
-                    longitude=77.74,
-                    zoom=17,
-                    pitch=50,
-                ),
-                layers=[selected_layers],
-            )
+    return selected_layers
+
+def create_map(selected_layers):
+    st.pydeck_chart(
+        pdk.Deck(
+            map_style=None,
+            initial_view_state=get_initial_view_state(),
+            layers=[selected_layers],
         )
+    )
+    
+
+if __name__=='__main__':
+    # dtm_path = './data/n12_e077_1arc_v3.tif'
+    # seg = RasterSegmentation(dtm_path=dtm_path)
+    file_path = './output/low_lying_pts.csv'
+    seg = StaticSegmentation(file_path=file_path)
+    df = seg.segment()
+    layers = get_map_layers(df)
+    selected_layers = select_layers(layers)
+    if selected_layers:
+        create_map(selected_layers)
     else:
         st.error("Please choose at least one layer above.")
