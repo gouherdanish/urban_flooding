dab88ce79acc17ca6d2d209703ba5a5893fe5340
Gouher Danish <gouherdanishiitkgp@gmail.com>
2024-09-20 20:54:41 +0530

    Streamlit app modified - added map layer for low lying areas and added sidebar control

--- app.py
+++ app.py
@@ -1,10 +1,43 @@
 from segmentation.static_segmentation import StaticSegmentation
 
 import streamlit as st
+import pydeck as pdk
 
 if __name__=='__main__':
     # dtm_path = './data/n12_e077_1arc_v3.tif'
     file_path = './output/low_lying_pts.csv'
     seg = StaticSegmentation(file_path=file_path)
     df = seg.segment()
-    st.map(df,latitude='y',longitude='x',size=5)
+    # st.map(df,latitude='y',longitude='x',size=5)
+
+    ALL_LAYERS = {
+        "Low Lying Areas":
+            pdk.Layer(
+                "ScatterplotLayer",
+                data=df,
+                get_position="[x, y]",
+                get_color="[200, 30, 0, 160]",
+                get_radius=5,
+            ),
+    }
+    st.sidebar.markdown("### Map Layers")
+    selected_layers = [
+        layer
+        for layer_name, layer in ALL_LAYERS.items()
+        if st.sidebar.checkbox(layer_name, True)
+    ]
+    if selected_layers:
+        st.pydeck_chart(
+            pdk.Deck(
+                map_style=None,
+                initial_view_state=pdk.ViewState(
+                    latitude=12.96,
+                    longitude=77.73,
+                    zoom=17,
+                    pitch=50,
+                ),
+                layers=[selected_layers],
+            )
+        )
+    else:
+        st.error("Please choose at least one layer above.")
