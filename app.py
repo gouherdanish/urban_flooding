63cee5c088bbeb54bbf8ac819875ebe7fe6b07f6
Gouher Danish <gouherdanishiitkgp@gmail.com>
2024-09-25 12:30:36 +0530

    Added comments and removed prints

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def get_initial_view_state(village_poly):
     return pdk.ViewState(
         latitude=centroid_lat,
         longitude=centroid_lon,
-        zoom=15,
+        zoom=12,
         pitch=50,
     )
 
@@ -59,31 +59,57 @@ def create_map(selected_layers,village_poly):
     
 
 if __name__=='__main__':
-    # dtm_path = './data/n12_e077_1arc_v3.tif'
-    # seg = RasterSegmentation(dtm_path=dtm_path)
-    low_points_file_path = './output/low_lying_pts.shp'
-    bangalore_villages_file_path = './data/20_Bengaluru_Urban/20_Bengaluru _Urban.shp'
+    """
+    Main entry point for streamlit app
+
+    Step 1: Create low lying points data using suitable Image Segmentation Process
+        a) Raster Segmentation - performs segmentation on raster afresh
+        >>> dtm_path = './data/n12_e077_1arc_v3.tif'
+        >>> seg = RasterSegmentation(dtm_path=dtm_path)
+
+        b) Static Segmentation Data - reads already segmented and saved local data
+        >>> low_points_file_path = './output/low_lying_pts.shp'
+        >>> seg = StaticSegmentation(file_path=low_points_file_path)
+
+        Note: Online segmentation using raster is expensive operation and causes latency on user's end.
+        Therefore, the app uses static segmentation currently by default
+
+    Step 2: Read Bangalore Villages Shpfile Data
     
+    Step 3: Filter low lying points data for user-selected village
+
+    Step 4: Create Streamlit Map for Selected Village and underlying low lying points
+    """
+
+    # Image Segmentation
+    low_points_file_path = './output/low_lying_pts.shp'
     seg = StaticSegmentation(file_path=low_points_file_path)
     low_points_df = seg.segment()
 
+    # Bangalore Villages
+    bangalore_villages_file_path = './data/20_Bengaluru_Urban/20_Bengaluru _Urban.shp'
     village_polygons_df = gpd.read_file(bangalore_villages_file_path).to_crs('EPSG:4326')
+    
+    # User-selected Village
     options = village_polygons_df['KGISVill_2'].values
     selected_village = st.selectbox(
         label='Which village in Bengaluru do you live in ?',
         options=options,
         placeholder="Select your village..."
     )
-    print(selected_village)
-
     selected_village_df = village_polygons_df.loc[village_polygons_df['KGISVill_2']==selected_village,['geometry']].reset_index(drop=True).iloc[-1:,:]
+    
+    # Filtering Low lying points inside selected village
     low_points_in_village_df = gpd.sjoin(low_points_df,selected_village_df,predicate='within',how='inner').drop('index_right',axis=1).reset_index(drop=True)
-    print(selected_village_df)
-    print(low_points_in_village_df)
-
     village_poly = selected_village_df.values[0][0]
+
+    # Creating Layers for Streamlit Map
     layers = get_map_layers(low_points_in_village_df,village_poly)
+
+    # Providing Option to select/un-select layers
     selected_layers = select_layers(layers)
+    
+    # Creating Streamlit Map
     if selected_layers:
         create_map(selected_layers,village_poly)
     else:
