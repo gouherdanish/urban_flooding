5e656e809f3bd2ce4cb6a1fb4314b4baad2df4d0
Gouher Danish <gouherdanishiitkgp@gmail.com>
2024-09-25 12:49:35 +0530

    Added comments and removed prints

--- app.py
+++ app.py
@@ -6,75 +6,7 @@ from shapely.geometry import mapping
 from segmentation.raster_segmentation import RasterSegmentation
 from segmentation.static_segmentation import StaticSegmentation
 
-def get_initial_view_state(village_poly):
-    """
-    Creates initial view state for the map layer.
-    Directs streamlit to got to the centroid of the given village
-    Shows the map at zoom level 12 (default)
-    """
-    centroid_lon,centroid_lat = village_poly.centroid.x,village_poly.centroid.y
-    return pdk.ViewState(
-        latitude=centroid_lat,
-        longitude=centroid_lon,
-        zoom=12,
-        pitch=50,
-    )
-
-def get_map_layers(low_points_in_village_df,selected_village_poly): 
-    """
-    Creates village boundary in a format that is accepted by Pydeck
-    Creates polygon layer for showing Village Boundary on map
-    Creates point layer for showing Low lying points on map
-    """
-    village_boundary = [list(coord) for coord in selected_village_poly.exterior.coords] 
-    village_polygon_layer = pdk.Layer(
-        "PolygonLayer",
-        data=[village_boundary],
-        get_polygon='-',
-        stroked=False,
-        filled=True,
-        get_fill_color="[255, 0, 100, 160]",
-        pickable=True
-    )
-    low_points_layer = pdk.Layer(
-        "ScatterplotLayer",
-        data=low_points_in_village_df.drop('geometry',axis=1),
-        get_position="[x, y]",
-        get_color="[200, 30, 0, 160]",
-        get_radius=10,
-        pickable=True
-    )
-    return  {
-        "Low Lying Areas":low_points_layer,
-        "Village Boundaries":village_polygon_layer,
-    }
-
-def select_layers(layers):
-    """
-    Creates a sidebar with radio buttons to enable selecting from `layers`
-    """
-    st.sidebar.markdown("### Map Layers")
-    selected_layers = [
-        layer
-        for layer_name, layer in layers.items()
-        if st.sidebar.checkbox(layer_name, True)
-    ]
-    return selected_layers
-
-def create_map(selected_layers,village_poly):
-    """
-    Creates a Pydeck chart for the selected layers which is dispayed on Streamlit map
-    """
-    st.pydeck_chart(
-        pdk.Deck(
-            map_style=None,
-            initial_view_state=get_initial_view_state(village_poly),
-            layers=[selected_layers],
-        )
-    )
-    
-
-if __name__=='__main__':
+class App:
     """
     Main entry point for streamlit app
 
@@ -97,6 +29,75 @@ if __name__=='__main__':
     Step 4: Create Streamlit Map for Selected Village and underlying low lying points
     """
 
+    def get_initial_view_state(self,village_poly):
+        """
+        Creates initial view state for the map layer.
+        Directs streamlit to got to the centroid of the given village
+        Shows the map at zoom level 12 (default)
+        """
+        centroid_lon,centroid_lat = village_poly.centroid.x,village_poly.centroid.y
+        return pdk.ViewState(
+            latitude=centroid_lat,
+            longitude=centroid_lon,
+            zoom=13,
+            pitch=50,
+        )
+
+    def get_map_layers(self,low_points_in_village_df,selected_village_poly): 
+        """
+        Creates village boundary in a format that is accepted by Pydeck
+        Creates polygon layer for showing Village Boundary on map
+        Creates point layer for showing Low lying points on map
+        """
+        village_boundary = [list(coord) for coord in selected_village_poly.exterior.coords] 
+        village_polygon_layer = pdk.Layer(
+            "PolygonLayer",
+            data=[village_boundary],
+            get_polygon='-',
+            stroked=False,
+            filled=True,
+            get_fill_color="[255, 0, 100, 160]",
+            pickable=True
+        )
+        low_points_layer = pdk.Layer(
+            "ScatterplotLayer",
+            data=low_points_in_village_df.drop('geometry',axis=1),
+            get_position="[x, y]",
+            get_color="[200, 30, 0, 160]",
+            get_radius=10,
+            pickable=True
+        )
+        return  {
+            "Low Lying Areas":low_points_layer,
+            "Village Boundaries":village_polygon_layer,
+        }
+
+    def select_layers(self,layers):
+        """
+        Creates a sidebar with radio buttons to enable selecting from `layers`
+        """
+        st.sidebar.markdown("### Map Layers")
+        selected_layers = [
+            layer
+            for layer_name, layer in layers.items()
+            if st.sidebar.checkbox(layer_name, True)
+        ]
+        return selected_layers
+
+    def create_map(self,selected_layers,village_poly):
+        """
+        Creates a Pydeck chart for the selected layers which is dispayed on Streamlit map
+        """
+        st.pydeck_chart(
+            pdk.Deck(
+                map_style=None,
+                initial_view_state=self.get_initial_view_state(village_poly),
+                layers=[selected_layers],
+            )
+        )
+    
+
+if __name__=='__main__':
     # Image Segmentation
     low_points_file_path = './output/low_lying_pts.shp'
     seg = StaticSegmentation(file_path=low_points_file_path)
@@ -120,13 +121,14 @@ if __name__=='__main__':
     village_poly = selected_village_df.values[0][0]
 
     # Creating Layers for Streamlit Map
-    layers = get_map_layers(low_points_in_village_df,village_poly)
+    app = App()
+    layers = app.get_map_layers(low_points_in_village_df,village_poly)
 
     # Providing Option to select/un-select layers
-    selected_layers = select_layers(layers)
+    selected_layers = app.select_layers(layers)
     
     # Creating Streamlit Map
     if selected_layers:
-        create_map(selected_layers,village_poly)
+        app.create_map(selected_layers,village_poly)
     else:
         st.error("Please choose at least one layer above.")
